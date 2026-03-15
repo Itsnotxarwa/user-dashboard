2c398a3752171bf799038cb5f9e878e23a543806
itsnotxarwa <marwa.kchkh@gmail.com>
2026-03-15 14:55:24 +0100

    remove login page keep onlu user dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard';
 import History from './components/history';
-import Login from './Login';
-import ProtectedRoute from './ProtectedRoute';
 
 function App() {
 
@@ -10,8 +8,7 @@ function App() {
     <div>
       <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/user-dashboard" element={<Dashboard />} />
         <Route path="/history" element={<History />} />
       </Routes>
     </Router>
