bbda134fe25d43de5acca61d2cde323595a63dd6
itsnotxarwa <marwa.kchkh@gmail.com>
2026-04-27 16:34:20 +0100

    update2

--- src/components/agents-components/AgentsOverview.jsx
+++ src/components/agents-components/AgentsOverview.jsx
@@ -1,7 +1,9 @@
 import AgentsTable from "./AgentsTable";
 
 export default function AgentsOverview({agents}) {
-    <div className="min-h-screen bg-[rgba(3,44,166,0.09)] flex max-w-7xl mx-auto">
-        <AgentsTable agents={agents} />
-    </div>
+    return(
+        <div className="min-h-screen bg-[rgba(3,44,166,0.09)] flex max-w-7xl mx-auto">
+            <AgentsTable agents={agents} />
+        </div>
+    )
 }
