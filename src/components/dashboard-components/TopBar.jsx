ef53d706f5f6dc2058dadeb067d03384e2923bb2
itsnotxarwa <marwa.kchkh@gmail.com>
2026-05-18 16:30:38 +0100

    update 3

--- src/components/dashboard-components/TopBar.jsx
+++ src/components/dashboard-components/TopBar.jsx
@@ -1,6 +1,6 @@
 import { ChevronRight, Plus } from "lucide-react";
 
-export default function TopBar({activeNav, isActive, setShowCreateModal}) {
+export default function TopBar({activeNav, isActive, setShowCreateCampaign}) {
     
     return(
         <div className="h-14 bg-white border-b flex items-center px-7 py-2 gap-4 shrink-0
@@ -13,7 +13,7 @@ export default function TopBar({activeNav, isActive, setShowCreateModal}) {
                 </span>
             </div>
 
-            {activeNav?.label === "Tableau de bord" && (
+            {activeNav?.name === "Tableau de bord" && (
             <div className="flex items-center gap-1.5 ml-auto">
                 <span className={`w-1.5 h-1.5 rounded-full
                     ${isActive? "bg-[#22c55e] shadow-[0_0_5px_#22c55e]" : "bg-[#6b7280] shadow-[0_0_5px_#6b7280]"}`}></span>
@@ -23,12 +23,12 @@ export default function TopBar({activeNav, isActive, setShowCreateModal}) {
             </div>
             )}
 
-            {activeNav?.label === "Campagnes" && (
+            {activeNav?.name === "Campagnes" && (
             <button
             className="flex items-center gap-2 px-5 py-2.5 rounded-xl ml-auto
             text-xs font-bold text-white transition-all bg-[#032ca6]
             border border-[#032ca6] shadow-[0_4px_14px_rgba(3,44,166,0.25)] cursor-pointer"
-            onClick={() => setShowCreateModal(true)}>
+            onClick={() => setShowCreateCampaign(true)}>
                 <Plus size={18} />
                 Ajouter une campagne
             </button>
