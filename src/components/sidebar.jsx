f2a21b6f138bb79c73efb74942d4c48376ca1d94
itsnotxarwa <marwa.kchkh@gmail.com>
2026-06-18 17:04:02 +0100

    update

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -41,9 +41,9 @@ export default function Sidebar() {
 
 
     return(
-            <aside className="flex flex-col w-64 px-4 md:px-6 h-screen overflow-y-auto
+            <aside className="flex flex-col bg-white w-64 px-4 md:px-6 h-screen overflow-y-auto
             py-8 transition-all duration-300 ease-in-out justify-between dark:bg-[#161b22] 
-            shrink-0 border-r border-[#032ca6]80 dark:border-[#21262d] fixed top-0 left-0 bottom-0 z-50 ">
+            shrink-0 border-r border-[rgba(3,44,166,0.05)] dark:border-[#21262d] fixed top-0 left-0 bottom-0 z-50 ">
                 <div>
                 <div className="flex items-center justify-center">
                     <img src={Logo} alt="Mazia" className="w-30" />
